698062c4fbb22e6b2a7c94e2b4660f86692ea3c7
ismail baoud <ibaoud46@egmail.com>
2025-10-23 10:19:14 +0100

    Création du contrôleur REST FournisseurController avec les endpoints: GET /api/v0/fournisseurs (affichage de tous les fournisseurs), POST /api/v0/fournisseurs (ajout d'un fournisseur), PUT /api/v0/fournisseurs/{id} (modification d'un fournisseur), DELETE /api/v0/fournisseurs/{id} (suppression d'un fournisseur)

--- src/main/java/org/ismail/Tricol/controller/FournisseurController.java
+++ src/main/java/org/ismail/Tricol/controller/FournisseurController.java
@@ -1,13 +1,46 @@
 package org.ismail.Tricol.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ismail.Tricol.model.Fournisseur;
+import org.ismail.Tricol.service.FournisseurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v0/fournisseurs")
 public class FournisseurController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Hello World";
+    private FournisseurService fournisseurService;
+
+    public void setFournisseurService(FournisseurService fournisseurService) {
+        this.fournisseurService = fournisseurService;
     }
+
+    @PostMapping
+    public Fournisseur save(@RequestBody Fournisseur fournisseur) {
+        fournisseurService.saveFournisseur(fournisseur);
+        return fournisseur;
+    }
+
+    @GetMapping
+    public List<Fournisseur> findAll() {
+        return fournisseurService.findAllFournisseurs();
+    }
+
+    @GetMapping("/{id}")
+    public Fournisseur findById(@PathVariable("id") Long id) {
+        return fournisseurService.findFournisseurById(id).orElseThrow(() -> new IllegalArgumentException("Fournisseur avec l'ID " + id + " non trouvé"));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFournisseur(@PathVariable("id") Long id) {
+        fournisseurService.deleteFournisseurById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Fournisseur updateFournisseur(@PathVariable("id") Long id, @RequestBody Fournisseur fournisseur) {
+        return fournisseurService.updateFournisseur(id, fournisseur);
+    }
+
 }
